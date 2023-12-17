92fc6c473c590891836680c9a96ca9c6c7a147f6
Bhavneet Singh <97750271+BhavneetSingh7@users.noreply.github.com>
2023-12-17 16:40:22 +0530

    Build pipeline (#4)
    * linting job added
    
    * Update lint.yaml
    
    * Fix py version
    
    * Fixed flake8 issue
    
    * test case added
    
    * Unit test action pipeline added
    
    * Merged lint and test pipelines
    
    * Fixed pipeline
    
    * Build dist pipeline added
    
    * Updated build pipeline
    
    ---------
    
    Co-authored-by: Bhavneet Singh <bhavneetsingh@Bhavneets-MacBook-Air.local>

---
+++ ci-scripts/build-lambda.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo Building lambda zip file
+
+poetry --version
+poetry install --without dev
+poetry run pip list
+poetry run pip freeze > requirements.txt
+
+mkdir dist
+cp -r src/* dist
+poetry run pip --no-cache-dir install -r requirements.txt  --target dist
+
+rm requirements.txt
+zip -qr deployment.zip dist
+rm -rf dist
+du deployment.zip
+echo Build successful!
